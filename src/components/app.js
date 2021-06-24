882b2b0f5d03241ae8de8a2fad5388cb0343745d
Senn <shaunsenn2019@gmail.com>
2021-06-23 20:09:26 -0600

    6/23/2021

--- src/components/app.js
+++ src/components/app.js
@@ -1,11 +1,27 @@
 import React, { Component } from 'react';
+import Logo from "./logo"
 
 export default class App extends Component {
   render() {
     return (
       <div className='app'>
-        <h1>DevCamp React Starter</h1>
-        <h2>React Redux Router</h2>
+        <div className="container">
+        <div class="logo">
+          <Logo/>
+        </div>
+
+        <div class="search">
+          <input placeholder="daily smarty"/>
+        </div>
+
+        <div class="recent">
+          <ul>
+            <li>post</li>
+            <li>post</li>
+            <li>post</li>
+          </ul>
+        </div>
+        </div>
       </div>
     );
   }
