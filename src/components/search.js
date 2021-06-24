ab45621540659e6c7b25e37ba9bfd1e1538714bc
Senn <shaunsenn2019@gmail.com>
2021-06-23 21:26:32 -0600

    handleFormSubmit

--- src/components/search.js
+++ src/components/search.js
@@ -1,14 +1,32 @@
 import React, { Component } from "react";
+import { Field, reduxForm } from "redux-form";
 
 
 class Search extends Component {
+
+    handleFormSubmit = function({query}) {
+        console.log("trying to handle submit for query", query);
+        //navigate to a new route.
+    }
+
+    
+    renderInput(field) {
+        return <input type="text" placeholder="Search Daily Smarty" {...field.input} />
+    }
+
     render() {
+
+        const { handleSubmit } = this.props;
         return (
-            <form>
-                <input placeholder="search bar component"/>
+            <form className="search" onSubmit={handleSubmit(this.handleFormSubmit.bind(this))}>
+                <Field name="query" component={this.renderInput}/>
             </form>
         )
     }
 }
+
+Search = reduxForm({
+    form: 'search'
+})(Search);
 
 export default Search;
